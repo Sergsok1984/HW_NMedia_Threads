a42a592da42359c4e86ef06b0f80065ef326181d
Sergei Sokolov <sergsok1984@gmail.com>
2023-04-10 21:30:50 +0300

    add HW to Seminar5 Async

--- android/app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
+++ android/app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
@@ -3,9 +3,15 @@ package ru.netology.nmedia.repository
 import ru.netology.nmedia.dto.Post
 
 interface PostRepository {
-    fun getAll(): List<Post>
-    fun likeById(id: Long): Post
-    fun unlikeById(id: Long): Post
-    fun save(post: Post)
-    fun removeById(id: Long)
+
+    fun getAllAsync(callback: Callback<List<Post>>)
+    fun saveAsync(post: Post, callback: Callback<Post>)
+    fun removeByIdAsync(id: Long, callback: Callback<Unit>)
+    fun likeByIdAsync(id: Long, callback: Callback<Post>)
+    fun unlikeByIdAsync(id: Long, callback: Callback<Post>)
+
+    interface Callback<T> {
+        fun onSuccess(value: T)
+        fun onError(e: Exception)
+    }
 }
